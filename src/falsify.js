a96336d3f013446c950b75fa9943405581da0088
wizarddevweb <145326008+wizarddevweb@users.noreply.github.com>
2023-10-27 01:06:44 +0000

    Update 3 files with solution

--- src/falsify.js
+++ src/falsify.js
@@ -2,4 +2,11 @@ export default function falsify(func) {
   // should return a function that returns the opposite boolean value
   // of the given `func`
   // see falsify.test.js for test cases
+  // return function (...args) {
+  //   const f1 = func.bind(this);
+  //   return !f1(...args);
+  // };
+  return function () {
+    return !func.apply(this, arguments);
+  };
 }
